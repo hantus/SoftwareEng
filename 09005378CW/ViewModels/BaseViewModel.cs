dbcf0e57eb0826eaccf349234296ddae3b146af3
Piotr Handkowski <phandkowski@yahoo.com>
2018-10-24 11:47:24 +0100

    Initial commit

---
+++ 09005378CW/ViewModels/BaseViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09005378CW.ViewModels
+{
+    class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnChanged(string name)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ 09005378CW/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09005378CW.ViewModels
+{
+    class MainWindowViewModel : BaseViewModel
+    {
+    }
+}
